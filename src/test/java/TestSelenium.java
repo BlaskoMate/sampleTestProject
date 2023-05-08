151325d371717eb4597bf070d5a98c254810ced6
blaskoMate <blaskomate3@gmail.com>
2023-05-08 12:01:49 +0200

    Fix: add junit test annotation to test

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -1,9 +1,11 @@
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestSelenium {
-    public static void main(String[] args){
+    @Test
+    public void openDemoPage(){
         System.setProperty("/webdriver.chrome.driver","/src/test/resources/drivers/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://demo.guru99.com/");
