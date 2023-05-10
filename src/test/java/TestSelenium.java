fdc0edf9b124d7ed736aee3a598ba658c78258de
blaskoMate <blaskomate3@gmail.com>
2023-05-10 17:34:55 +0200

    Feat: run chrome in headless mode

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -2,12 +2,15 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestSelenium {
     @Test
     public void openDemoPage(){
         System.setProperty("/webdriver.chrome.driver","/src/test/resources/drivers/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
         driver.get("https://demo.guru99.com/");
         WebElement element = driver.findElement(By.xpath("//div[@id='site-name']"));
         System.out.println(element.getText());
